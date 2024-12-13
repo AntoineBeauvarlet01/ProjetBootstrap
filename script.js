fb5f160fddb2ea718397bf6defe6afd4b0b7a6ce
AntoineBeauvarlet01 <antoine.beauvarlet@ensea.fr>
2024-12-13 10:12:29 +0100

    ça fonctionne trust

--- script.js
+++ script.js
@@ -1,42 +1,27 @@
-const listeGens = [
-	{
-		title: "Antoine BEAUVARLET",
-		year: 2002,
-		imageUrl:
-			"https://media.licdn.com/dms/image/v2/D4E03AQFGdQMeagwCAQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1693301648944?e=1738195200&v=beta&t=Jd79TpXHLT5takDIFlQdj2mmS3bO2K4dZJgy2Rl0e-s",
-	},
-	{
-		title: "Nicolas Sarkozy",
-		year: 1955,
-		imageUrl:
-			"https://meridian-site.s3.us-east-2.amazonaws.com/wp-content/uploads/2015/04/Nicolas_Sarkozy.jpg",
-	},
-	{
-		title: "Jean Lassalle",
-		year: 1955,
-		imageUrl:
-			"https://engagement-animaux.fr/wp-content/uploads/2022/02/1200px-Jean_Lassalle_02_V2.jpg",
-	},
-	{
-		title: "Jean-Luc Mélenchon",
-		year: 1951,
-		imageUrl:
-			"https://i.f1g.fr/media/cms/704x396_cropupscale/2024/11/28/4ef1ea8437c28cbdd2bf2a877cdda5a176d595f5e89bef20c0d0c13a573ba2f5.jpg",
-	},
-	{
-		title: "François Hollande",
-		year: 1954,
-		imageUrl:
-			"https://scontent-cdg4-2.xx.fbcdn.net/v/t39.30808-6/319502820_2157277084454220_8216667403573444768_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=L-_0WV2TMT8Q7kNvgEC554D&_nc_zt=23&_nc_ht=scontent-cdg4-2.xx&_nc_gid=AIHouTmJg-m3VtKBUfbdKok&oh=00_AYBqQHbMhmNEDCis64GnSSzM5d-B9vRYxZNoWkFiNPQFOQ&oe=674FBA7F",
-	},
-	{
-		title: "Philippe Poutou",
-		year: 1967,
-		imageUrl:
-			"https://www.francebleu.fr/s3/cruiser-production-eu3/2024/06/fae00e8d-042b-46d9-bc0f-0f518b8ca3b0/1200x680_sc_poutou.webp",
-	},
+// initialize variable
+let carsList
 
-]
+fetch("http://localhost:3000/api/cars", {
+	method: "GET",
+	headers: {
+		"x-api-key": "azerty",
+		"Content-Type": "application/json",
+		Accept: "application/json",
+	},
+})
+	.then((res) => {
+		if (!res.ok) {
+			console.log("your API isn't working !!!")
+		}
+		res.json().then((data) => {
+			console.log(data)
+			carsList = data // Mise à jour de la liste des voitures avec les données récupérées
+			writeDom()  // APRÈS que les données aient été récupérées 
+		})
+	})
+	.catch((error) =>
+		console.error("Erreur lors de la récupération des voitures :", error)
+	)
 
 /*  DOM ELEMENTS  */
 const articleContainer = document.querySelector(".row")
@@ -165,3 +150,41 @@ editBtnList.forEach((btn, index) => {
 		})
 	})
 })
+function writeDom() {
+    carsList.forEach((car) => {
+        const articleContainer = document.querySelector(".row")
+        articleContainer.innerHTML += `
+        <article class="col">
+        <div class="card shadow-sm">
+            <img src="${car.carImage}" alt="${car.carName}" class="card-img-top" />
+            <div class="card-body">
+            <h3 class="card-title">${car.carName}</h3>
+                <p class="card-text">${car.carYear}</p>
+                <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                        <button 
+                            type="button" 
+                            class="btn btn-sm btn-outline-secondary view" 
+                            data-bs-toggle="modal" 
+                            data-bs-target="#exampleModal"
+                            data-view-id="${car.id}"
+                            >
+                                View
+                        </button>
+                        <button 
+                            type="button" 
+                            class="btn btn-sm btn-outline-secondary edit" 
+                            data-bs-toggle="modal" 
+                            data-bs-target="#exampleModal"
+                            data-edit-id="${car.id}"
+                            >
+                                Edit
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </article>
+    `
+    })
+}
